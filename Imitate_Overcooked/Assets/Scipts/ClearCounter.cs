703bdf6735a34355e087cdec481856a46cf332e9
Povet34 <70003696+Povet34@users.noreply.github.com>
2026-01-25 21:30:16 +0900

    Add kitchen object transfer logic between counters
    Introduced references between ClearCounter and KitchenObject to allow transferring kitchen objects between counters. Added methods for setting, clearing, and checking kitchen objects on counters, and updated the scene to support these changes. Also added a testing flag to trigger object transfer with a key press.

--- Imitate_Overcooked/Assets/Scipts/ClearCounter.cs
+++ Imitate_Overcooked/Assets/Scipts/ClearCounter.cs
@@ -4,10 +4,54 @@ public class ClearCounter : MonoBehaviour
 {
     [SerializeField] KitchenObjectSO kitchenObjectSO;
     [SerializeField] Transform counterTopPoint;
+    [SerializeField] ClearCounter secoundClearCounter;
+    [SerializeField] bool testing;
+
+    KitchenObject kitchenObject;
+
+
+    private void Update()
+    {
+        if (testing && Input.GetKeyDown(KeyCode.T))
+        {
+            if (kitchenObject != null)
+            {
+                kitchenObject.SetClearCounter(secoundClearCounter);
+            }
+        }
+    }
 
     public void Interact()
     {
-        Transform obj =  Instantiate(kitchenObjectSO.prefab, counterTopPoint);
-        obj.localPosition = Vector3.zero;
+        if (kitchenObject == null)
+        {
+            Transform obj = Instantiate(kitchenObjectSO.prefab, counterTopPoint);
+            obj.GetComponent<KitchenObject>().SetClearCounter(this);
+        }
+    }
+
+    public Transform GetKitchenObjectFollowTransform()
+    {
+        return counterTopPoint;
+    }
+
+    public void SettKitchenObject(KitchenObject kitchenObject)
+    {
+        this.kitchenObject = kitchenObject;
+    }
+
+    public KitchenObject GetKitchenObject()
+    {
+        return kitchenObject;
+    }
+
+    public void ClearKitchenObject()
+    {
+        kitchenObject = null;
+    }
+
+    public bool HasKitchenObject()
+    {
+        return kitchenObject != null;
     }
 }
--- Imitate_Overcooked/Assets/Scipts/KitchenObject.cs
+++ Imitate_Overcooked/Assets/Scipts/KitchenObject.cs
@@ -3,4 +3,25 @@ using UnityEngine;
 public class KitchenObject : MonoBehaviour
 {
     [SerializeField] KitchenObjectSO kitchenObjectSO;
+
+    ClearCounter clearCounter;
+
+    public void SetClearCounter(ClearCounter clearCounter)
+    {
+        if(this.clearCounter != null)
+        {
+            this.clearCounter.ClearKitchenObject();
+        }
+
+        this.clearCounter = clearCounter;
+        clearCounter.SettKitchenObject(this);
+
+        transform.parent = clearCounter.GetKitchenObjectFollowTransform();
+        transform.localPosition = Vector3.zero;
+    }
+
+    public ClearCounter GetClearCounter()
+    {
+        return clearCounter;
+    }
 }
