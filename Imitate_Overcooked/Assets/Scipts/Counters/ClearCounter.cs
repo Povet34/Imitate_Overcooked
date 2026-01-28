9e18abff7911747abf45cbbc7622e0cc9af77379
LimJaesang <wotkd34@naver.com>
2026-01-28 21:45:18 +0900

    Add plate ingredient transfer logic to ClearCounter
    Enhanced ClearCounter to allow ingredient transfer from player to counter when the counter holds a plate and the player holds an ingredient. Also updated SampleScene to adjust object position.

--- Imitate_Overcooked/Assets/Scipts/Counters/ClearCounter.cs
+++ Imitate_Overcooked/Assets/Scipts/Counters/ClearCounter.cs
@@ -19,11 +19,21 @@ public class ClearCounter : BaseCounter
             if (player.HasKitchenObject())
             {
                 //Counter에 있는 KitchenObject를 플레이어가 들고있는 접시에 담는다.
-                if(player.GetKitchenObject().TryGetPlate(out var plateObject))
+                if (player.GetKitchenObject().TryGetPlate(out var plateObject))
                 {
-                    if(plateObject.TryAddIngredient(GetKitchenObject().GetKitchenObjectSO()))
+                    if (plateObject.TryAddIngredient(GetKitchenObject().GetKitchenObjectSO()))
                     {
                         GetKitchenObject().DestroySelf();
+                    }
+                }
+                else //Plate를 가지고있지 않고 다른걸 가지고 있을 때
+                {
+                    if (GetKitchenObject().TryGetPlate(out plateObject))
+                    {
+                        if (plateObject.TryAddIngredient(player.GetKitchenObject().GetKitchenObjectSO()))
+                        {
+                            player.GetKitchenObject().DestroySelf();
+                        }
                     }
                 }
             }
