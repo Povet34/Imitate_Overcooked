bccf7c516a52255562a23512e3fc26604db2a7fe
LimJaesang <wotkd34@naver.com>
2026-01-30 09:57:27 +0900

    Add dynamic ingredient icons to recipe UI
    Introduced a container and icon template to DeliveryManagerSingleUI for displaying ingredient icons dynamically based on the selected recipe. Updated the scene to include the new UI elements and linked them in the script. The UI now clears and repopulates ingredient icons when a new recipe is set.

--- Imitate_Overcooked/Assets/Scipts/UI/DeliveryManagerSingleUI.cs
+++ Imitate_Overcooked/Assets/Scipts/UI/DeliveryManagerSingleUI.cs
@@ -1,13 +1,34 @@
 using System;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DeliveryManagerSingleUI : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI recipeNameText;
+    [SerializeField] Transform container;
+    [SerializeField] Image iconTemplate;
+
+    private void Awake()
+    {
+        iconTemplate.gameObject.SetActive(false);
+    }
 
     public void SetRecipeSO(RecipeSO recipeSO)
     {
         recipeNameText.text = recipeSO.recipeName;
+        
+        foreach(Transform child in container)
+        {
+            if(child == iconTemplate.transform) continue;
+            Destroy(child.gameObject);
+        }
+
+        foreach(var kitchenObjectSO in recipeSO.kitchenObjectSos)
+        {
+            Image iconImage = Instantiate(iconTemplate, container);
+            iconImage.gameObject.SetActive(true);
+            iconImage.sprite = kitchenObjectSO.sprite;
+        }
     }
 }
